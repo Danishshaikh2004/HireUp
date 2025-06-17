c0c00e858a992b1aa39ddf35faf5dfeaa0757a5b
Danish Shaikh <skhdanish.8.4.5.2@gmail.com>
2025-06-17 23:34:09 +0530

    Updated frontend .env config and backend live setup

--- server/app.js
+++ server/app.js
@@ -18,6 +18,7 @@ app.use(express.json()); // Enable JSON parsing
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: "test",
 })
   .then(() => console.log("✅ MongoDB connected successfully"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
